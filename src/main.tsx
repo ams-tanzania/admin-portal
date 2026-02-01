37bc7d4785ed4d0e9aa5b65b78b3f32d9e2308ba
sensaj <jafarsalum07@gmail.com>
2026-02-01 08:25:17 +0300

    Removed unneccessary codes

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App.tsx";
 import { HashRouter } from "react-router-dom";
 
-const basename = import.meta.env.DEV ? "/" : "/admin-portal/";
-
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <StrictMode>
