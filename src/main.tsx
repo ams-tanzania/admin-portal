4a1899a4d1b981c460e5e8af1591b7dd75935447
sensaj <jafarsalum07@gmail.com>
2026-02-01 06:05:55 +0300

    changed BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-const basename = import.meta.env.DEV ? "/" : "/WeMovies/";
+const basename = import.meta.env.DEV ? "/" : "/admin-portal/";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={basename}>
+  <HashRouter basename={basename}>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
 );
